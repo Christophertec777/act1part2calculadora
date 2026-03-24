a414fdfb7776eb29fa63194d54814ae45971db2a
Gerry-zqqq14 <al07013940@tecmilenio.mx>
2026-03-23 19:22:07 -0700

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,11 @@
+def suma(a, b):
+    return a + b
+
 def resta(a, b):
     return a - b
 
 def main():
+    print("Suma:", suma(5, 3))
     print("Resta:", resta(5, 3))
 
 if __name__ == "__main__":
