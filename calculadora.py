b6b2476e07dda9ca9f950f9ab31446513ad9e88a
sergio2114 <al07034209@tecmilenio.mx>
2026-03-23 19:22:54 -0700

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,21 @@
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def multiplicacion(a, b):
+    return a * b
+
 def division(a, b):
     if b == 0:
         return "Error: división por cero"
     return a / b
 
 def main():
+    print("Suma:", suma(5, 3))
+    print("Resta:", resta(5, 3))
+    print("Multiplicación:", multiplicacion(5, 3))
     print("División:", division(5, 3))
 
 if _name_ == "_main_":
